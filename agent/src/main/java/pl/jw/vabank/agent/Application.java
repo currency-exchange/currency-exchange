6ad77a20a1fe415e3e4d0b73fea08d4986a97bdf
jacek.wisniewski <jacek.wisniewski.max@gmail.com>
2015-06-05 16:28:27 +0200

    New modules.

---
+++ agent/src/main/java/pl/jw/vabank/agent/Application.java
@@ -0,0 +1,50 @@
+package pl.jw.vabank.agent;
+
+import javax.sql.DataSource;
+
+import org.jooq.SQLDialect;
+import org.jooq.impl.DefaultConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		return new SimpleDriverDataSource();
+	}
+	
+	@Bean
+	public DefaultConfiguration jooq() {
+		DefaultConfiguration defaultConfiguration = new DefaultConfiguration();
+		defaultConfiguration.setSQLDialect(SQLDialect.FIREBIRD);
+		defaultConfiguration.setDataSource(newDataSource);
+		return defaultConfiguration;
+	}
+	// http://docs.spring.io/spring/docs/current/spring-framework-reference/html/scheduling.html
+
+	// SchedulerFactoryBean
+	// @Bean
+	// public SchedulingConfigurer threadPoolTaskExecutor() {
+	// ThreadPoolTaskExecutor threadPoolTaskExecutor = new
+	// ThreadPoolTaskExecutor();
+	// threadPoolTaskExecutor.setCorePoolSize(1);
+	// threadPoolTaskExecutor.setMaxPoolSize(1);
+	// threadPoolTaskExecutor.setQueueCapacity(1);
+	//
+	// return threadPoolTaskExecutor;
+	// }
+	//
+	// TaskScheduler
+	//
+	// Trigger PeriodicTrigger
+}
---
+++ agent/src/main/java/pl/jw/vabank/agent/ChangesExporter.java
@@ -0,0 +1,8 @@
+package pl.jw.vabank.agent;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ChangesExporter {
+
+}

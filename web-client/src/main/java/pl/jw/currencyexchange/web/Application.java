80f53615bdc52b77e156b83f11cbfb793a55bc7a
Jacek Wiśniewski <jbwisniewski@gmail.com>
2015-08-27 22:29:42 +0200

    Web client impl.

--- web-client/src/main/java/pl/jw/currencyexchange/web/Application.java
+++ web-client/src/main/java/pl/jw/currencyexchange/web/Application.java
@@ -9,7 +9,7 @@ import pl.jw.currencyexchange.model.mongo.ConfigurationMongo;
 @SpringBootApplication
 @Import(ConfigurationMongo.class)
 public class Application {
-
+	// https://spring.io/blog/2015/01/12/spring-and-angular-js-a-secure-single-page-application
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);

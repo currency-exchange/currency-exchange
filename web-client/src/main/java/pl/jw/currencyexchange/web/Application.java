04306eed0941d21fecd9e4c96f495ce5b50bd0c6
Jacek Wiśniewski <jbwisniewski@gmail.com>
2015-08-30 19:40:46 +0200

    Wbe prototype - done.

--- web-client/src/main/java/pl/jw/currencyexchange/web/Application.java
+++ web-client/src/main/java/pl/jw/currencyexchange/web/Application.java
@@ -2,13 +2,41 @@ package pl.jw.currencyexchange.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 import pl.jw.currencyexchange.model.mongo.ConfigurationMongo;
 
 @SpringBootApplication
 @Import(ConfigurationMongo.class)
 public class Application {
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/")
+			.permitAll().anyRequest().authenticated().and()
+			.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class).csrf()
+					.csrfTokenRepository(csrfTokenRepository());
+			;
+		}
+
+		private CsrfTokenRepository csrfTokenRepository() {
+			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+			repository.setHeaderName("X-XSRF-TOKEN");
+			return repository;
+		}
+	}
+
 	// https://spring.io/blog/2015/01/12/spring-and-angular-js-a-secure-single-page-application
 	public static void main(String[] args) {
 
